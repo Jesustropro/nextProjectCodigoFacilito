375ae8c13375d78b8f40e5d7c1ca8d8648786863
Jesustropro <jdmoraelpro1@gmail.com>
2023-07-20 02:19:17 -0300

    test  fix dislikes in vercell

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -134,17 +134,13 @@ export default function CardQuote({ quotes, deleteQuote }: QuoteParams) {
     if (dislike) {
       if (countLikes && countLikes > 0) {
         setCountLikes(countLikes - 1);
-        try {
-          const result = await fetch(`/api/auth/quotes?id=${_id}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ quotes, countLikes: -1 }),
-          });
-          quotes.likesCount = quotes.likesCount - 1;
-        } catch (error) {
-          console.error(error);
-        }
-        return;
+
+        const result = await fetch(`/api/auth/quotes?id=${_id}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ quotes, countLikes: -1 }),
+        });
+        quotes.likesCount = quotes.likesCount - 1;
       }
       try {
         const result = await fetch(`/api/auth/liked?id=${session?.user?._id}`, {
