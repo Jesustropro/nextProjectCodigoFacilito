56c9820cd6282f7f80dabe7a737319b8f09ccd2b
Jesustropro <jdmoraelpro1@gmail.com>
2023-07-28 15:13:12 -0300

    test share quotes with url page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -61,6 +61,7 @@ export default function CardQuote({
 
           const data = {
             files: filesArray,
+            url: "https://next-project-codigo-facilito.vercel.app/",
           };
           console.log(data, filesArray);
 
